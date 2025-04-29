0286d4709ee990c1632d5f7385a837b6b7544060
Josue Castro <jcastro@ksmpartners.onmicrosoft.com>
2025-04-29 18:31:57 -0400

    added conveyor belt style works fine we need our svg icons in this as next steps

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,37 @@
-// src/pages/index.js
 import React from 'react';
 import Layout from '../components/Layout/Layout';
 import { Link } from 'gatsby';
 import { motion } from 'framer-motion';
 import DisplayDiv2 from '../components/Div2Home/Div2Home';
-// import './HomePage.css';
 import '../pages/Homepage/HomePage.css';
 import '../styles/global.css';
 import "primereact/resources/themes/lara-light-blue/theme.css"
+import TickerBelt from '../components/TickerBelt/TickerBelt';
 // THIS IS THE HOMEPAGE JS (GATSBY NEEDS IT NAMED INDEX.JS)
 const HomePage = () => {
+
+    //logos for TICKER BELT (these should be later stored somewhere else like assets move this out of here)
+    const allLogos = [
+        // TypeScript
+        "https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg",
+        // Node.js
+        "https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg",
+        // npm
+        "https://upload.wikimedia.org/wikipedia/commons/d/db/Npm-logo.svg",
+        // Bootstrap
+        "https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg",
+        // Socket.IO
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Kubernetes_logo_without_workmark.svg/800px-Kubernetes_logo_without_workmark.svg.png",
+        "https://cdn.worldvectorlogo.com/logos/docker.svg",
+        "https://static-00.iconduck.com/assets.00/terraform-icon-1803x2048-hodrzd3t.png",
+        
+        "https://logos-world.net/wp-content/uploads/2021/08/Amazon-Web-Services-AWS-Logo.png"
+      ];
+
+
+
+
+
     return (
         <Layout>
             <motion.div
@@ -27,6 +49,8 @@ const HomePage = () => {
                 </header>
                 
                 <DisplayDiv2/>
+                <TickerBelt logos={allLogos}/>
+
 
                 <section className="features-section">
                     <div className="feature">
