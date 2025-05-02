d307d721189bf9e2887b2189f1a0a0856932c023
Peter Van Beever <petervanbeever@gmail.com>
2025-05-02 12:45:24 -0400

    Updated Case Studies

--- src/pages/case-studies.js
+++ src/pages/case-studies.js
@@ -2,59 +2,76 @@
 import ReactFlow, { Background, Controls, MiniMap } from 'reactflow';
 import ReactFlowScene from '../components/ReactFlowScene/ReactFlowScene.js';
 import 'reactflow/dist/style.css';
+import './grids.css';
 import React from 'react';
-// import Header from '../Header/Header';
-// import Footer from '../Footer/Footer';
-// import Navbar from '../NavBar/NavBar';
-// import './Layout.css'; // Layout styles if needed
 import TopMegaMenu from '../components/TopMegaMenu/TopMegaMenu';
-// import HeroSection from '../HeroSection/HeroSection';
-// import AnimatedIcon from "../icons/AnimatedIcon";
-// // import LayersIcon from "../icons/LayersIcon";
-// import { SettingsGearIcon } from '../icons/SettingsGearIcon';
-// import { RocketIcon } from '../icons/RocketIcon';
-
+import Footer from '../components/Footer/Footer';
 import Layout from '../components/DynamicGradientBackground/DynamicGradientBackground.js';
+
+// Menu Items
 import Div1 from '../components/CaseStudy/Div1/Div1.js';
 import Div2 from '../components/CaseStudy/Div2/Div2.js';
 import Div3 from '../components/CaseStudy/Div3/Div3.js';
+import Div4 from '../components/CaseStudy/Div4/Div4.js';
+import Div5 from '../components/CaseStudy/Div5/Div5.js';
+import Div6 from '../components/CaseStudy/Div6/Div6.js';
 
+// Content Items
+import Domino from '../components/CaseStudy/domino-sce-integration.js';
+import SelfService from '../components/CaseStudy/web-services-for-self-service-order-managemen.js';
+import DataAggregation from '../components/CaseStudy/market-monitoring-data-collection-and-aggregation.js';
+import RealTime from '../components/CaseStudy/real-time-power-flow-calculation-engine.js';
+import GxPDevOps from '../components/CaseStudy/gxp-delivery-automation-with-azure-devops.js';
+import DataPipelining from '../components/CaseStudy/data-engineering-and-pipelining.js';
+import AutomatedClinical from '../components/CaseStudy/automated-and-controlled-clinical-data-analysis.js';
+
+// Gradients for THIS Page
 const customGradients = [
-    { angle: 70, colors: ["#9193FF", "#C0DBFC"] },
-    { angle: 0, colors: ["#c7d3ec", "#F1F2F3"] },
-    { angle: 180, colors: ["#bbd4e5", "#F1F2F3"] },
-    { angle: 45, colors: ["#e3ecf1", "#F1F2F3"] },
-    { angle: 0, colors: ["#c7d3ec", "#F1F2F3"] },
-    { angle: 180, colors: ["#01b4af", "#F1F2F3"] },
+    { angle: 45, colors: ["#dde6ff", "#eef5ff"] },
+    { angle: 0, colors: ["#b7acff", "#0039aa"] },
+    { angle: 0, colors: ["#dde6ff", "#e6dad5"] },
+    { angle: 180, colors: ["#eef5ff", "#dde6ff"] },
+    { angle: 45, colors: ["#dde6ff", "#eef5ff"] },
+    { angle: 0, colors: ["#dde6ff", "#e6dad5"] },
+    { angle: 180, colors: ["#eef5ff", "#dde6ff"] },
+    { angle: 45, colors: ["#dde6ff", "#eef5ff"] },
+    { angle: 0, colors: ["#dde6ff", "#e6dad5"] },
+    { angle: 180, colors: ["#eef5ff", "#dde6ff"] },
+    { angle: 45, colors: ["#dde6ff", "#eef5ff"] },
+    { angle: 0, colors: ["#dde6ff", "#e6dad5"] },
+    { angle: 180, colors: ["#eef5ff", "#dde6ff"] },
+    { angle: 180, colors: ["#0039aa", "#0039aa"] },
   ];
+
+
+// #e6dad5
+// #6176b5
 
 
 const IndexPage = () => {
   return (
-    <Layout gradients={customGradients}>
-    <TopMegaMenu />
-    <Div1 />
-    <Div2 />
-    <Div3 />
-    {[...Array(8)].map((_, i) => (
-        <div
-          key={i}
-          style={{
-            height: '100vh',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            fontSize: '3rem',
-            fontWeight: 'bold',
-            borderBottom: '1px solid #ccc',
-          }}
-        >
-          Div {i + 1}
-        </div>
-      ))}
-      <ReactFlowScene />
-    </Layout>
-    
+    <div className='background-layer'>
+        <Layout gradients={customGradients}>
+            <TopMegaMenu />
+            <div className='content-layer'>
+                <Div1 />
+                <Div2 />
+                <Div3 />
+                <Div4 />
+                {/* <Div5 /> */}
+                {/* <Div6 /> */}
+                {/* <Domino /> */}
+                <DataAggregation />
+                <SelfService />
+                <RealTime  />
+                <AutomatedClinical />
+                <GxPDevOps />
+                <DataPipelining />
+            </div>
+            <div className="flowchart-layer "><ReactFlowScene /></div>
+            <Footer />
+        </Layout>
+    </div>
   );
 };
 
